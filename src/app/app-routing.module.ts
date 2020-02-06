2219a0ac14141f48fc3ed1f4bff213307b334c55
Mahesh Sabnis <sabnis_m@hotmail.com>
2020-02-06 12:28:04 +0530

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/routingapp/app.home.component';
+import { AboutComponent } from './components/routingapp/app.about.component';
+import { ContactComponent } from './components/routingapp/app.contact.component';
+import { StudentFormComponent } from './components/studentformcomponent/app.studentform.component';
 
+// define the route table
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'about/:id', component:AboutComponent},
+  {path:'contact', component:ContactComponent, 
+    children: [
+      {path:'student', component: StudentFormComponent}
+  ]},
+  {path:'lazy', loadChildren:'./../lib/app.lazy.module#LazyModule'},
+  {path:'**', redirectTo:''} // path other than the route path('','about','contact')
+]; // redirectTo the '' (default)
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)], // registering the RouteTable at application root level
+  exports: [RouterModule] // exports the RouterModule with Route Table to AppModule 
 })
 export class AppRoutingModule { }
