ef0b2f093145041cf4c143944a96a22e1cd39d6e
Mahesh Sabnis <sabnis_m@hotmail.com>
2020-02-06 12:26:39 +0530

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,22 @@ import { AppComponent } from './app.component';
 import {SimpleComponent} from './components/simplecomponent/app.simple.component';
 import { StudentFormComponent } from './components/studentformcomponent/app.studentform.component';
 import { StudentReactiveFormComponent } from './components/studentreactiveformcomponent/app.studentreactiveform.component';
+import { UniversityComponent } from './components/masterdetails/app.university.component';
+import { StudentComponent } from './components/masterdetails/app.student.component';
+import { UtilityComponent } from './components/utilitycomponent/app.utility.component';
+import { UtilityService } from './services/app.utility.service';
+import { HttpServiceComponent } from './components/httpservicecomponent/app.httpservice.component';
+import {ServiceModule} from './../servmodule/app.serv.module';
+import { SecureCallComponent } from './components/securecallcomponent/app.securecall.component';
+import { UniversitySenderComponent} from './components/masterdetailsservice/app.universitysender.component'
+import { StudentReceiverComponent } from './components/masterdetailsservice/app.studentreceiver.component';
+import { DataGridComponent } from './directives/app.datagrid.component';
+import { ColorDirective } from './directives/app.color.directive';
+import { ColorDirectiveComponent } from './components/colordirectivecomponent/app.colordirective.component';
+import { HomeComponent } from './components/routingapp/app.home.component';
+import { AboutComponent } from './components/routingapp/app.about.component';
+import { ContactComponent } from './components/routingapp/app.contact.component';
+import { MainComponent } from './components/routingapp/app.main.component';
 // declarations: of the type array, used to declare all components, pipes 
 // and directives in current application to be loaded
 // imports: of the type array, used to import standard modules and custom 
@@ -18,13 +34,18 @@ import { StudentReactiveFormComponent } from './components/studentreactiveformco
 @NgModule({
   declarations: [
     AppComponent, SimpleComponent, StudentFormComponent,
-    StudentReactiveFormComponent
+    StudentReactiveFormComponent, UniversityComponent,
+     StudentComponent, UtilityComponent, 
+     HttpServiceComponent, SecureCallComponent,
+     UniversitySenderComponent, StudentReceiverComponent,
+     DataGridComponent, ColorDirective, ColorDirectiveComponent,
+     HomeComponent, AboutComponent, ContactComponent, MainComponent
   ],
   imports: [
     BrowserModule, FormsModule, ReactiveFormsModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [StudentReactiveFormComponent]
+    AppRoutingModule, ServiceModule
+  ], //  AppRoutingModule --> Load the Route Table at AppModule Level aka root level
+  providers: [UtilityService],
+  bootstrap: [MainComponent]
 })
 export class AppModule { }
